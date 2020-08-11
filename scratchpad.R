8f6c617bbd1f674516494e34d5f3fe7a84f8f8df
Jacob Sowder <jacobsowder@gmail.com>
2020-08-10 18:07:01 -0700

    start tibble

--- scratchpad.R
+++ scratchpad.R
@@ -5,4 +5,12 @@ library(xml2)
 dfp_blog <-
   1:30 %>% 
   paste0("https://www.dataforprogress.org/blog?page=", .) %>%
-  sapply(read_html, simplify = F, USE.NAMES = T)
+  sapply(read_html, simplify = F, USE.NAMES = T) %>% 
+  sapply(xml_find_all, '//a[@class="BlogList-item-title"]', simplify = F)
+
+dfp_posts_base <-
+  dfp_blog %>%
+  flatten() %>% 
+  sapply(xml_attr, "href") %>% 
+  paste0("https://www.dataforprogress.org", .) %>% 
+  tibble("url" = .)
