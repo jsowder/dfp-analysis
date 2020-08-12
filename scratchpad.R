0c0a51951c92880e1e568da6198f234a65c7e597
Jacob Sowder <jacobsowder@gmail.com>
2020-08-12 07:43:54 -0700

    slow down scraping

--- scratchpad.R
+++ scratchpad.R
@@ -1,16 +1,24 @@
 library(tidyverse)
 library(magrittr)
 library(xml2)
+library(rvest)
+
+safe_read <-
+  possibly(
+  function(url){
+    Sys.sleep(3)
+    message(paste("Parsing", url))
+    read_html(url)
+  },
+  "404")
 
 dfp_blog <-
   1:30 %>% 
   paste0("https://www.dataforprogress.org/blog?page=", .) %>%
-  sapply(read_html, simplify = F, USE.NAMES = T) %>% 
-  sapply(xml_find_all, '//a[@class="BlogList-item-title"]', simplify = F)
+  sapply(safe_read, simplify = F, USE.NAMES = T) %>% 
 
 dfp_posts_base <-
   dfp_blog %>%
   flatten() %>% 
   sapply(xml_attr, "href") %>% 
   paste0("https://www.dataforprogress.org", .) %>% 
-  tibble("url" = .)
