884def7ab2ea2c3f8d473def4c44ac42de58788f
Jacob Sowder <jacobsowder@gmail.com>
2020-08-12 08:33:51 -0700

    get post contents

--- scratchpad.R
+++ scratchpad.R
@@ -5,12 +5,12 @@ library(rvest)
 
 safe_read <-
   possibly(
-  function(url){
-    Sys.sleep(3)
-    message(paste("Parsing", url))
-    read_html(url)
-  },
-  "404")
+    function(url){
+      Sys.sleep(1)
+      message(paste("Parsing", url))
+      read_html(url)
+    },
+    "404")
 
 dfp_blog <-
   1:30 %>% 
@@ -24,4 +24,43 @@ dfp_posts_base <-
   sapply(xml_attr, "href") %>% 
   paste0("https://www.dataforprogress.org", .) %>% 
   tibble("url" = .) %>% 
-  mutate(raw_src = map(url, read_html))
+  mutate(raw_src = map(url, safe_read))
+
+dfp_posts <-
+  dfp_posts_base %>% 
+  mutate(
+    title = 
+      raw_src %>% 
+      map_chr(~.x %>% 
+                as_xml_document() %>% 
+                html_node(".BlogItem-title") %>% 
+                html_text()
+      ),
+    pub_date = 
+      raw_src %>% 
+      map_chr(~.x %>% 
+                as_xml_document() %>% 
+                html_node(".Blog-meta-item--date") %>% 
+                html_text()
+      ),
+    authors = 
+      raw_src %>% 
+      map(~.x %>% 
+            as_xml_document() %>% 
+            html_node('[data-layout-label="Post Body"]') %>%
+            html_node('p') %>% 
+            html_nodes('strong') %>% 
+            html_text()
+      ),
+    content = 
+      raw_src %>% 
+      map(~.x %>% 
+            as_xml_document() %>% 
+            html_node('[data-layout-label="Post Body"]') %>%
+            html_nodes('p') %>%
+            tail(-1) %>% 
+            html_text()
+      ),
+  ) %T>%
+  glimpse()
+
